c76db3328544a8059c2e25a28e5cf28e346c5217
Rabearivony Loïc <122444363+space9cowboy@users.noreply.github.com>
2023-04-17 01:24:05 +0200

    création des boutons dynamiques et récupération des données API catégories

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -4,6 +4,10 @@ au code. Dans ce cas, l'API est hébergée localement sur la même machine que l
 `http://localhost:5678/api/`. */
 const api = "http://localhost:5678/api/";
 
+let categoryId = "";
+let categories = [];
+
+
 /**
  * Cette fonction récupère les données d'un point de terminaison de l'API 
  * pour les travaux et les enregistre dans la console, tout en gérant les erreurs éventuelles.
@@ -15,9 +19,11 @@ async function fetchWorksApi() {
         .then((res) => res.json())
         .then((data) => (works = data)) ;
         console.log("La récupération des données de l'API est ok");
-        console.log(works);
 
+       /* appel a la fonction workdisplays */
         worksDisplay(works);
+        const btnCategories = getBtnCategories(works);
+        btnCategoriesFilter(btnCategories);
     }
    
     catch (err) {
@@ -35,7 +41,9 @@ données de l'API ne sont récupérées et enregistrées dans la console qu'une 
 ce qui permet d'éviter les problèmes potentiels liés à la synchronisation des données de l'API.
 tout problème potentiel lié à la synchronisation de la demande d'API. */
 
-window.addEventListener("load", () => fetchWorksApi());
+window.addEventListener("load", () => {fetchWorksApi();
+fetchCategoriesApi();
+});
 
 function worksDisplay(data) {
 
@@ -52,11 +60,63 @@ function worksDisplay(data) {
             `;
             works.appendChild(workElement);
           });
+}
+
+/* récupération des données API des catégories */
+async function fetchCategoriesApi() {
+    try {
+        await fetch(api + "categories")
+        .then((res) => res.json())
+        .then((data) => (categories = data)) ;
+        console.log("La récupération des données de l'API est ok");
+        console.log(categories);
+       /* appel a la fonction workdisplays */
+        
+    }
+   
+    catch (err) {
+        console.log("erreur fetchCategoriesApi", err);
+    }
+
+}
+
+function getBtnCategories(data) {
+
+    const categoryNameSet = new Set(works.map((work) => work.category.name));
+    const uniqueCategoryNameSet = Array.from(categoryNameSet);
+    return uniqueCategoryNameSet;
 
 }
 
 
-  
+function btnCategoriesFilter(categories) {
+    const blockFilter = document.querySelector(".filter");
+    const buttonsTous = document.createElement("button");
+    buttonsTous.classList.add("btn","active");
+    buttonsTous.textContent = "Tous";
+    blockFilter.appendChild(buttonsTous);
+
+    const allButtons = [buttonsTous, ...categories.map((categoryName) => {
+        const buttonAutres = document.createElement("button");
+        buttonAutres.classList.add("btn");
+        buttonAutres.textContent = categoryName;
+        blockFilter.appendChild(buttonAutres); 
+        return buttonAutres;
+    })];
+
+    allButtons.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            categoryId = event.target.textContent;
+            allButtons.forEach((button) => {
+                button.classList.remove("active");
+            });
+            event.target.classList.add("active");
+            worksDisplay();
+        });
+    });
+}
+
+
   
   
 
